6db8ffd3fb80d72f2921064c96ef07e51accd980
xushiping12138 <32231711+xushiping12138@users.noreply.github.com>
2020-11-13 11:08:28 +0800

    test-pagic

---
+++ README.md
@@ -0,0 +1,79 @@
+---
+next: introduction/README.md
+---
+
+# TypeScript 入门教程
+
+[![Actions Status](https://github.com/xcatliu/typescript-tutorial/workflows/gh-pages/badge.svg)](https://github.com/xcatliu/typescript-tutorial/actions)
+
+从 JavaScript 程序员的角度总结思考，循序渐进的理解 TypeScript。
+
+## 关于本书
+
+- [在线阅读](https://xushiping12138.github.io/)
+- [GitHub 地址][GitHub]
+- 作者：[xcatliu](https://github.com/xushiping12138/)
+- [加入官方 QQ 群 767142358](https://jq.qq.com/?_wv=1027&k=5nkkFCl)
+- 本网站使用 [Pagic](https://github.com/xcatliu/pagic) 构建
+
+本书是作者在学习 [TypeScript] 后整理的学习笔记。
+
+随着对 TypeScript 理解的加深和 TypeScript 社区的发展，本书也会做出相应的更新，欢迎大家 [Star 收藏][GitHub]。
+
+- 发现文章内容有问题，可以直接在页面下方评论
+- 对项目的建议，可以[提交 issue](https://github.com/xcatliu/typescript-tutorial/issues/new) 向作者反馈
+- 欢迎直接提交 pull-request 参与贡献
+
+## 为什么要写本书
+
+TypeScript 虽然有[官方手册][Handbook]及其[非官方中文版][中文手册]，但是它每一章都希望能详尽的描述一个概念，导致前面的章节就会包含很多后面才会学习到的内容，而有些本该一开始就了解的基础知识却在后面才会涉及。如果是初学者，可能需要阅读多次才能理解。所以它更适合用来查阅，而不是学习。
+
+与官方手册不同，本书着重于从 JavaScript 程序员的角度总结思考，循序渐进的理解 TypeScript，希望能给大家一些帮助和启示。
+
+由于一些知识点与官方手册重合度很高，本书会在相应章节推荐直接阅读中文手册。
+
+## 关于 TypeScript
+
+[TypeScript] 是 JavaScript 的一个超集，主要提供了**类型系统**和**对 ES6 的支持**，它由 Microsoft 开发，代码[开源于 GitHub](https://github.com/Microsoft/TypeScript) 上。
+
+它的第一个版本发布于 2012 年 10 月，经历了多次更新后，现在已成为前端社区中不可忽视的力量，不仅在 Microsoft 内部得到广泛运用，而且 Google 开发的 [Angular](https://angular.io/) 从 2.0 开始就使用了 TypeScript 作为开发语言，[Vue](https://vuejs.org/) 3.0 也使用 TypeScript 进行了重构。
+
+## 适合人群
+
+本书适合以下人群
+
+- 熟悉 JavaScript，至少阅读过一遍[《JavaScript 高级程序设计》](https://book.douban.com/subject/10546125/)
+- 了解 ES6，推荐阅读 [ECMAScript 6 入门]
+- 了解 Node.js，会用 npm 安装及使用一些工具
+- 想了解 TypeScript 或者想对 TypeScript 有更深的理解
+
+本书**不适合**以下人群
+
+- 没有系统学习过 JavaScript
+- 已经能够很熟练的运用 TypeScript
+
+## 评价
+
+> 《TypeScript 入门教程》全面介绍了 TypeScript 强大的类型系统，完整而简洁，示例丰富，比官方文档更易读，非常适合作为初学者学习 TypeScript 的第一本书。
+>
+> —— [阮一峰](https://github.com/ruanyf)
+
+## 版权许可
+
+本书采用「保持署名—非商用」创意共享 4.0 许可证。
+
+只要保持原作者署名和非商用，您可以自由地阅读、分享、修改本书。
+
+详细的法律条文请参见[创意共享](http://creativecommons.org/licenses/by-nc/4.0/)网站。
+
+## 相关资料
+
+- [TypeScript 官网][TypeScript]
+- [Handbook]（[中文版][中文手册]）
+- [ECMAScript 6 入门]
+
+[GitHub]: https://github.com/xcatliu/typescript-tutorial
+[TypeScript]: http://www.typescriptlang.org/
+[Handbook]: http://www.typescriptlang.org/docs/handbook/basic-types.html
+[中文手册]: https://zhongsp.gitbook.io/typescript-handbook/
+[ECMAScript 6 入门]: http://es6.ruanyifeng.com/
---
+++ pagic.config.tsx
@@ -0,0 +1,105 @@
+// @deno-types="https://deno.land/x/pagic@v0.8.4/src/types/react/v16.13.1/react.d.ts"
+import React from 'https://dev.jspm.io/react@16.13.1';
+
+export default {
+  srcDir: '.',
+  exclude: ['examples'],
+  root:'/doc/',
+  theme: 'docs',
+  plugins: ['sidebar', 'prev_next', 'gitalk', 'ga'],
+  title: 'TypeScript 入门教程',
+  description: '从 JavaScript 程序员的角度总结思考，循序渐进的理解 TypeScript。',
+  github: 'https://github.com/xcatliu/typescript-tutorial',
+  head: <link rel="icon" type="image/png" href="/favicon.png" />,
+  nav: [
+    {
+      text: '赞助作者',
+      link: 'https://github.com/xcatliu/buy-me-a-coffee',
+      target: '_blank',
+      popover: (
+        <>
+          <img src="/assets/wechat.jpg" width="256" style={{ marginRight: '1rem', verticalAlign: 'top' }} />
+          <img src="/assets/alipay.jpg" width="256" style={{ verticalAlign: 'top' }} />
+        </>
+      )
+    }
+  ],
+  sidebar: {
+    '/': [
+      {
+        link: 'introduction/README.md',
+        children: [
+          'introduction/what-is-typescript.md',
+          'introduction/get-typescript.md',
+          'introduction/hello-typescript.md'
+        ]
+      },
+      {
+        link: 'basics/README.md',
+        children: [
+          'basics/primitive-data-types.md',
+          'basics/any.md',
+          'basics/type-inference.md',
+          'basics/union-types.md',
+          'basics/type-of-object-interfaces.md',
+          'basics/type-of-array.md',
+          'basics/type-of-function.md',
+          'basics/type-assertion.md',
+          'basics/declaration-files.md',
+          'basics/built-in-objects.md'
+        ]
+      },
+      {
+        link: 'advanced/README.md',
+        children: [
+          'advanced/type-aliases.md',
+          'advanced/string-literal-types.md',
+          'advanced/tuple.md',
+          'advanced/enum.md',
+          'advanced/class.md',
+          'advanced/class-and-interfaces.md',
+          'advanced/generics.md',
+          'advanced/declaration-merging.md',
+          'advanced/further-reading.md'
+        ]
+      },
+      {
+        link: 'engineering/README.md',
+        children: ['engineering/lint.md', 'engineering/compiler-options.md']
+      },
+      'thanks/README.md'
+    ]
+  },
+  tools: {
+    editOnGithub: true,
+    backToTop: true
+  },
+  tocAd: (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 192*128 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:192px;height:128px"
+     data-ad-client="ca-pub-8483371329009107"
+     data-ad-slot="6487368873"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>`
+      }}
+    />
+  ),
+  gitalk: {
+    clientID: '29aa4941759fc887ed4f',
+    clientSecret: '33e355efdf3a1959624506a5d88311145208471b',
+    repo: 'typescript-tutorial',
+    owner: 'xcatliu',
+    admin: ['xcatliu'],
+    pagerDirection: 'first'
+  },
+  ga: {
+    id: 'UA-45256157-14'
+  },
+  port: 8001
+};
